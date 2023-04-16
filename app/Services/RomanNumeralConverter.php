47e70e7751006cc4483686e9d0e815f7c463fcd5
Chris Weir <cweir132@gmail.com>
2023-04-16 19:05:22 +0100

    Updated converter and registered to container

--- app/Services/RomanNumeralConverter.php
+++ app/Services/RomanNumeralConverter.php
@@ -2,7 +2,34 @@
 
 namespace App\Services;
 
-class RomanNumeralConverter
+class RomanNumeralConverter implements IntegerConverterInterface
 {
+    private array $lookup = [
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1,
+    ];
 
+    public function convertInteger(int $integer): string
+    {
+        $returnValue = '';
+
+        foreach ($this->lookup as $roman => $value) {
+            $matches = intval($integer / $value);
+            $returnValue .= str_repeat($roman, $matches);
+            $integer = $integer % $value;
+        }
+
+        return $returnValue;
+    }
 }
